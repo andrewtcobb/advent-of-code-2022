a52f95bd72fb8029618af4e67917d480ef211a47
andrewtcobb <andrewtcobb94@gmail.com>
2022-12-06 09:31:09 -0500

    Add solve for day 6 -> part 1

---
+++ Day-6/Part1.rb
@@ -0,0 +1,15 @@
+require 'set'
+input = File.readlines("Day-6/input.txt", chomp: true)
+
+buffer = []
+input[0].chars.each_with_index do |char, i|
+  if buffer.size >= 4
+    if buffer.to_set.size == buffer.size
+      puts i
+      break
+    else
+      buffer.pop
+    end
+  end
+  buffer.prepend(char)
+end
