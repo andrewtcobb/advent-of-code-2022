698e855bebab67882052f741fe2237b8d17ad3ca
andrewtcobb <andrewtcobb94@gmail.com>
2022-12-09 16:31:19 -0500

    Refactor day 9 -> part 1 solve

--- Day-9/Part1.rb
+++ Day-9/Part1.rb
@@ -8,55 +8,30 @@ end
 
 input = File.readlines("Day-9/input.txt", chomp: true)
 
-tail_positions = Set.new
-tail_positions.add({x:0, y:0})
-current_head_position = {x:0, y:0}
-current_tail_position = {x:0, y:0}
+tail_positions = Set[x:0, y:0]
+current_head_position = { x: 0, y: 0 }
+current_tail_position = { x: 0, y: 0 }
 
 input.each do |line|
   direction, distance = line.split(' ')
 
-  case direction
-  when "R"
-    distance.to_i.times do
-      new_head_position = current_head_position.dup
+  distance.to_i.times do
+    new_head_position = current_head_position.dup
+    case direction
+    when "R"
       new_head_position[:x] += 1
-      if tail_too_far_away_from_head?(new_head_position, current_tail_position)
-        current_tail_position = current_head_position
-        tail_positions.add(current_tail_position)
-      end
-      current_head_position = new_head_position
-    end
-  when "L"
-    distance.to_i.times do
-      new_head_position = current_head_position.dup
+    when "L"
       new_head_position[:x] -= 1
-      if tail_too_far_away_from_head?(new_head_position, current_tail_position)
-        current_tail_position = current_head_position
-        tail_positions.add(current_tail_position)
-      end
-      current_head_position = new_head_position
-    end
-  when "U"
-    distance.to_i.times do
-      new_head_position = current_head_position.dup
+    when "U"
       new_head_position[:y] += 1
-      if tail_too_far_away_from_head?(new_head_position, current_tail_position)
-        current_tail_position = current_head_position
-        tail_positions.add(current_tail_position)
-      end
-      current_head_position = new_head_position
-    end
-  when "D"
-    distance.to_i.times do
-      new_head_position = current_head_position.dup
+    when "D"
       new_head_position[:y] -= 1
-      if tail_too_far_away_from_head?(new_head_position, current_tail_position)
-        current_tail_position = current_head_position
-        tail_positions.add(current_tail_position)
-      end
-      current_head_position = new_head_position
     end
+    if tail_too_far_away_from_head?(new_head_position, current_tail_position)
+      current_tail_position = current_head_position
+      tail_positions.add(current_tail_position)
+    end
+    current_head_position = new_head_position
   end
 end
 
